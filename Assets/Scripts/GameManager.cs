64daf5feee1912dfa3a5b653e317df063256a0f5
Ugosoft13 <ugosoft13@gmail.com>
2022-10-05 14:42:21 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager singleton;
+
+    private GroundPieceController[] allGroundPieces;
+    private PlayerController player;
+  
+
+
+    private void Start()
+    {
+        SetupNewLevel();
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    private void SetupNewLevel()
+    {
+        allGroundPieces = FindObjectsOfType<GroundPieceController>();
+    }
+
+    private void Awake()
+    {
+        if (singleton == null)
+            singleton = this;
+        else if (singleton != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        SetupNewLevel();
+    }
+
+    public void CheckComplete()
+    {
+        bool isFinished = true;
+
+        for (int i = 0; i < allGroundPieces.Length; i++)
+        {
+            if (allGroundPieces[i].isColored == false)
+            {
+                isFinished = false;
+                break;
+            }
+        }
+
+        if (isFinished)
+        {
+            StartCoroutine(waiter());
+            
+           
+
+        }
+
+           
+    }
+
+    private void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    IEnumerator waiter()
+    {
+        
+        //player.winParticle.Play();
+
+        //Wait for 2 seconds
+        yield return new WaitForSeconds(1.2f);
+        NextLevel();
+
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+        
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Maliyo-Project-6
+ This is excercise 6
